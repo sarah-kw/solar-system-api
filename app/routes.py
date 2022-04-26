51fe0e214b2e89942863a227dd321d33967a6ef2
sarah <skiyoko14@gmail.com>
2022-04-26 12:50:50 -0700

    refactor planet class to build dictionary of info, helper function for invalid id input

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort, make_response
 
 class Planet:
     def __init__(self, id, name, description, dist_from_star_km):
@@ -6,6 +6,15 @@ class Planet:
         self.name = name
         self.description = description
         self.dist_from_star_km = dist_from_star_km
+    
+    def make_dict(self):
+        planet_dict = {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "dist_from_star_km": self.dist_from_star_km
+            }
+        return planet_dict
 
 
 planet_list = [
@@ -20,31 +29,25 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 def get_planets():
     planet_response = []
     for planet in planet_list:
-        planet_response.append(
-            {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "dist_from_star_km": planet.dist_from_star_km
-            }
-        )
+        planet_response.append(planet.make_dict())
     return jsonify(planet_response)
 
-@planets_bp.route("/<id>", methods=["GET"])
-def get_one_planet(id):
+
+def handle_GET_id_requests(id):
     try:
         id = int(id)
     except ValueError:
-        return {"msg": f"Planet ID '{id}' is invalid. Requires int."}, 400
+        abort(make_response({"msg":f"Planet ID '{id}' is invalid. Requires int."}, 400))
+    return id
+
+@planets_bp.route("/<id>", methods=["GET"])
+def get_one_planet(id):
+    id = handle_GET_id_requests(id)
+    
     planet_return = None
     for planet in planet_list:
         if planet.id == id:
-            planet_return = {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "dist_to_star_km": planet.dist_from_star_km
-            }
+            planet_return = planet.make_dict()
     if not planet_return:
         return {"msg": f"Planet ID '{id}' does not exist"}, 404
     return jsonify(planet_return), 200
